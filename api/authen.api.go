8e3e0acace9346f91d15a624931013c74b7e74c0
tannyyhang <tannyyhang@gmail.com>
2021-06-22 13:31:21 +0700

    ep9 - Hash Password

--- api/authen.api.go
+++ api/authen.api.go
@@ -6,6 +6,7 @@ import (
 	"main/model"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 //ถ้าตอนนี้ ยังไม่ได้ใช้ก็ให้ void เอาไว้ก่อน โดยการใส่ _ นำหน้า
@@ -20,9 +21,35 @@ func SetupAuthenAPI(router *gin.Engine) {
 }
 
 // Login - login api
+
 func login(c *gin.Context) {
-	//เปลี่ยน 401 -> 200
 	c.JSON(200, gin.H{"result": "login"})
+	var user model.User
+
+	if c.ShouldBind(&user) == nil {
+		var queryUser model.User		
+		if err := db.GetDB().First(&queryUser, "username = ?", user.Username).Error; err != nil {							
+			c.JSON(200, gin.H{"result": "nok", "error": err})
+		}else if (checkPasswordHash(user.Password, queryUser.Password) == false){
+			c.JSON(200, gin.H{"result": "nok", "error": "invalid password"})
+		}else{			
+			c.JSON(200, gin.H{"result": "ok", "data": user})
+		}
+
+	} else {
+		c.JSON(401, gin.H{"status": "unable to bind data"})
+	}	
+}
+
+
+func checkPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+func hashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
 }
 
 
@@ -39,7 +66,7 @@ func register(c *gin.Context) {
 	var user model.User
 	if c.ShouldBind(&user) == nil{
 		//ถ้าการ Bind ไม่มี error
-		//user.Password, _ = hashPassword(user.Password)
+		user.Password, _ = hashPassword(user.Password)
 		user.CreatedAt = time.Now()
 		if err := db.GetDB().Create(&user).Error; err != nil{
 			c.JSON(200, gin.H{"result": "nok", "error": err})
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.2
-	gorm.io/driver/sqlite v1.1.4 // indirect
-	gorm.io/gorm v1.21.11 // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.21.11
 )
