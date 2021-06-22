89a9598d3ede0c3faacac678597326206e0d9805
tannyyhang <tannyyhang@gmail.com>
2021-06-22 14:06:59 +0700

    ep11 - Interceptor

--- api/product.api.go
+++ api/product.api.go
@@ -1,12 +1,27 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin")
+
+
+	// โดยปรกติ เราจะไม่ทำ func myInterceptor ไว้ในที่เดียวกับตัวที่ handler
+func myInterceptor(c *gin.Context){
+	token := c.Query("token")
+	if(token == "1234"){
+		c.Next() // ถ้า token ตรง c.Next() ก็คือการบอกว่า ไปต่อได้
+	}else{
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+		c.Abort()
+	}
+}
 
 func SetupProductAPI(router *gin.Engine) {
 	productAPI := router.Group("/api/v2")
 	{
 		// ตรงนี้เคยมี error เกิดขึ้นเนื่องจาก
-		productAPI.GET("/product", getProduct) //ตรงนี้เคยใช้ POST เลยเปลืี่ยนเป็น GET แทน (จะสังเกตุเห็นว่า่ ใช้ /product เหมือนกัน )
+		productAPI.GET("/product",myInterceptor, getProduct) //ตรงนี้เคยใช้ POST เลยเปลืี่ยนเป็น GET แทน (จะสังเกตุเห็นว่า่ ใช้ /product เหมือนกัน )
 		productAPI.POST("/product", createProduct) //ดังนั้น จะต้อง พิจารณา  GET กับ POST ให้ดี
 	}
 }
